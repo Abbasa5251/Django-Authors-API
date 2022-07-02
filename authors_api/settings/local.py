5ca03274442f0dcc28a8f0d7eccecc9772bd9b1f
Abbas Anandwala <abbasa5251@hotmail.com>
2022-07-03 03:06:55 +0530

    added redis, celery, flower

--- authors_api/settings/local.py
+++ authors_api/settings/local.py
@@ -10,6 +10,13 @@ SECRET_KEY = env(
 
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
 
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
+EMAIL_PORT = env("EMAIL_PORT")
+DEFAULT_EMAIL = "info@adevtutorials.com"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "ADev Authors"
+
 # DATABASES = {
 #     "default": {
 #         "ENGINE": "django.db.backends.sqlite3",
